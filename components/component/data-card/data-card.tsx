770c849fba15653b7985a7da79230db0939c9d4b
R0ma01 <romaine.brand@polymtl.ca>
2024-10-07 10:01:08 -0400

    fix

--- components/component/data-card/data-card.tsx
+++ components/component/data-card/data-card.tsx
@@ -84,7 +84,7 @@ const DataCard: React.FC<DataCardProps> = ({
         case DataCardType.SIMPLE_GRAPH:
             return (
                 <DataCardDiv title={content.title[lang]}>
-                    <div className="mt-5">
+                    <div className="mt-5 border border-black">
                         <GraphBox content={content.graphData[0]} year={year} />
                     </div>
                     <DescriptionComponent>
